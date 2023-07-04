743e1319a8eeeeb593e5849dfa92a8dcc477403a
Breno Santos <brenosantos145@gmail.com>
2023-07-03 22:01:53 -0300

    feat: creating the base of cli funcionality

--- cli/go.mod
+++ cli/go.mod
@@ -3,7 +3,13 @@ module github.com/breno5g/url-shortener-cli
 go 1.20
 
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/Baozisoftware/qrcode-terminal-go v0.0.0-20170407111555-c0650d8dff0f // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/urfave/cli/v2 v2.25.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 )
--- cli/main.go
+++ cli/main.go
@@ -3,8 +3,73 @@ Copyright © 2023 Breno Santos
 */
 package main
 
-import "github.com/breno5g/url-shortener-cli/cmd"
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	qrcodeTerminal "github.com/Baozisoftware/qrcode-terminal-go"
+
+	"github.com/urfave/cli/v2"
+)
+
+type UrlStruct struct {
+	Url string `json:"url"`
+}
 
 func main() {
-	cmd.Execute()
+	app := &cli.App{
+		Name:  "Url Shortener",
+		Usage: "Shorten your urls",
+		Commands: []*cli.Command{
+			{
+				Name:    "create",
+				Aliases: []string{"c"},
+				Usage:   "insert your URL",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "url",
+						Aliases:  []string{"u"},
+						Usage:    "insert your URL",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					url := c.String("url")
+
+					jsonStr := []byte(`{"url":"` + url + `"}`)
+					req, err := http.NewRequest("POST", "http://127.0.0.1:3001/url", bytes.NewBuffer(jsonStr))
+
+					req.Header.Set("Content-Type", "application/json")
+
+					client := &http.Client{}
+					res, err := client.Do(req)
+
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					defer res.Body.Close()
+
+					var decodedRes UrlStruct
+
+					json.NewDecoder(res.Body).Decode(&decodedRes)
+
+					obj := qrcodeTerminal.New()
+
+					obj.Get(decodedRes.Url).Print()
+
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
